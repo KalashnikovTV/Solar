76361da129216a659e32338e3f468478d3812a22
KalashnikovTV <max.malaschkov@mail.ru>
2020-03-05 21:46:06 +0300

    added sections

--- app/js/main.js
+++ app/js/main.js
@@ -7,5 +7,22 @@ $(function(){
 
     });
 
+    
+    $('.reviews__slider-big').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.reviews__slider-small'
+      });
+    $('.reviews__slider-small').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.reviews__slider-big',
+        focusOnSelect: true,
+        centerMode: true,
+        autoplay: 500,
+        prevArrow: '<button class="slick-prev slick-arrow"><img src="images/reviews/back.png" alt=""></button>', 
+        nextArrow: '<button class="slick-next slick-arrow"><img src="images/reviews/forward.png" alt=""></button>'
+    });
 
 });
