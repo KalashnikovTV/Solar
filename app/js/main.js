515d76d7b15443f3cee6091dfe7c7dfcf51ff796
KalashnikovTV <max.malaschkov@mail.ru>
2020-03-07 16:16:37 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -2,9 +2,31 @@ $(function(){
 
     $('.project__slider').slick({
         arrows: false,
-        slidesToShow: 3,
-        slidesToScroll: 3
-
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+              breakpoint: 1320,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+              }
+            },
+            {
+              breakpoint: 791,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 761,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+          ]
     });
 
     
@@ -15,14 +37,18 @@ $(function(){
         asNavFor: '.reviews__slider-small'
       });
     $('.reviews__slider-small').slick({
-        slidesToShow: 3,
+        slidesToShow: 5,
         slidesToScroll: 1,
         asNavFor: '.reviews__slider-big',
         focusOnSelect: true,
         centerMode: true,
         autoplay: 500,
         prevArrow: '<button class="slick-prev slick-arrow"><img src="images/reviews/back.png" alt=""></button>', 
-        nextArrow: '<button class="slick-next slick-arrow"><img src="images/reviews/forward.png" alt=""></button>'
+        nextArrow: '<button class="slick-next slick-arrow"><img src="images/reviews/forward.png" alt=""></button>',
+    });
+
+    $('.header__menu-btn').on('click', function(){
+      $('.menu > ul').slideToggle();
     });
 
 });
